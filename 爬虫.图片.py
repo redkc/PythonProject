4e0b76bb0bd44ac9b6b1e16c1e6e8021c24a5e54
redkc02 <752725323@qq.com>
2022-08-27 10:12:58 +0800

    爬取图片

--- 爬虫.图片.py
+++ 爬虫.图片.py
@@ -5,11 +5,23 @@
 # @File : 爬虫.图片.py
 # @Software: PyCharm
 import requests
-
+import os
 path = "C:/Users/redkc02/Desktop/PythonProject/abc.jpg"
 url = "https://ts1.cn.mm.bing.net/th/id/R-C.0e4bea43ecc474230d88ef73d4e99e82?rik=Uaf3iXNMPkQOaQ&riu=http%3a%2f%2fimg.mm4000.com%2ffile%2f0%2f4e%2f0713475320.jpg&ehk=BiFlSz%2fIz62YbBxZ3WH87dt8RhqKsb%2f2YE%2fCZxDtzkM%3d&risl=&pid=ImgRaw&r=0"
-r = requests.get(url)
-r.status_code
-with open(path,'wb') as f:
-    f.write(r.content)
-f.close()
+#root = "C://Users//redkc02//Desktop//PythonProject//"
+#path = root + url.split('/')[-1]
+try:
+    # if not os.path.exists(root):
+    #     os.mkdir(root)
+    # if not os.path.exists(path):
+    #     r = requests.get(url)
+    r = requests.get(url)
+    r.status_code
+    with open(path,'wb') as f:
+        f.write(r.content)
+        f.close()
+        print("文件保存成功")
+    # else:
+    #   print("文件已存在“）
+except:
+    print("爬取失败")
